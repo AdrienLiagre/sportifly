fc492e2098f569dda95c2cde90759810c1bd1761
Adrien Liagre <adrienliagre@hotmail.fr>
2015-10-27 14:46:35 +0100

    routes done with cecile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,35 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  namespace :account do
+    resource :dashboard, only: :show, controller: 'dashboard'
+  end
 
+  namespace :group do
+    scope ':group' do
+      resource :home, only: :show
+
+      resources :activities do
+        resources :bookings, only: [:new, :create, :destroy]
+      end
+
+      resources :newsfeeds, only: [:new, :create]
+      resources :users, only: [:show]
+
+      # resources :sports, only: [:index]
+      resources :locations, only: [:new, :create, :edit, :update, :destroy]
+      resources :location_reviews, only: [:create]
+
+      resources :bookings, only: [] do
+        resources :comments, only: [:create]
+      end
+
+      # resources :location_reviews, only: [:index, :new, :create]
+      # resources :comments, only: [:index, :new, :create]
+
+      # resources :bookings, only: [:index, :destroy]
+    end
+  end
 
 
 
