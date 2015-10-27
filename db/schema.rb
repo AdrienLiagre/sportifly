cf579a698d93e18722023d0184da6ba417b4e2b3
Adrien Liagre <adrienliagre@hotmail.fr>
2015-10-27 10:44:03 +0100

    creation location model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026174707) do
+ActiveRecord::Schema.define(version: 20151027094329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20151026174707) do
     t.string   "email_domain_name"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "postcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "newsfeeds", force: :cascade do |t|
