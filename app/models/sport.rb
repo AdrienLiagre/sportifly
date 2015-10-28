97d27e024bfcfdcdde10111a42924d8024e86334
Adrien Liagre <adrienliagre@hotmail.fr>
2015-10-28 19:06:36 +0100

    css done with antoine, to be continued

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -6,7 +6,7 @@ class Sport < ActiveRecord::Base
   validates :name, presence: true
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300>", thumb: "100x100>", large: "1000x200>"  }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
