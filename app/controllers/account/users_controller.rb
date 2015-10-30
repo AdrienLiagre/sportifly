640a2830ee9ab188d0a3e0009ee61a5950081ee2
Théophile Lemaitre <theophile.lemaitre@gmail.com>
2015-10-30 11:54:32 +0100

    i added the possibility to update one's profile

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -2,6 +2,7 @@ module Account
   class UsersController < ApplicationController
 
     def show
+      @user = current_user
     end
 
     def edit
@@ -9,9 +10,16 @@ module Account
     end
 
     def update
-
+      @user = current_user
+      # authorize @user
+      @user.update(user_params)
+      redirect_to account_dashboard_path
     end
 
+    private
 
+    def user_params
+      params.require(:user).permit(:name, :picture, :age, :crew, :phone_number)
+    end
   end
 end
