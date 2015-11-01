6e68d7dd955e1b01a5696b6222075d1f26852110
Adrien Liagre <adrienliagre@hotmail.fr>
2015-11-01 18:23:25 +0100

    style for home page activity presentation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
        image_tag 'jcdusse.png', class: "dashboard-avatar img-responsive"
      end
   end
+
+  def sport_picture(sport)
+    if @sport.picture.exists?
+       image_tag @sport.picture(:medium), class: "dashboard-sport-picture img-responsive"
+     else
+       image_tag 'jcdusse.png', class: "dashboard-sport-picture img-responsive"
+     end
+  end
 end
