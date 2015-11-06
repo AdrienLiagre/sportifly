bf32c52732a610ed7547202464e9e7390a8fa778
Adrien Liagre <adrienliagre@hotmail.fr>
2015-11-06 10:27:54 +0100

    Add activeadmin user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,21 @@
 ActiveAdmin.register User do
+  permit_params :email, :picture, :password, :password_confirmation, :name, :age, :crew, :phone_number
 
+  form do |f|
+    f.inputs "Details" do
+      f.semantic_errors # shows errors on :base
+      f.input :email
+      f.input :picture
+      if f.object.new_record?
+        f.input :password
+        f.input :password_confirmation
+      end
+      f.input :name
+      f.input :age
+      f.input :crew
+      f.input :phone_number
+    end
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
+    f.actions
+  end
 end
