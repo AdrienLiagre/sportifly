5b29e0c4a5afa748b0431c81c34afa119264c960
Théophile Lemaitre <theophile.lemaitre@gmail.com>
2015-11-12 12:30:38 +0100

    change in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110093950) do
+ActiveRecord::Schema.define(version: 20151111144645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(version: 20151110093950) do
     t.datetime "updated_at", null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "route"
   end
 
   create_table "merit_actions", force: :cascade do |t|
