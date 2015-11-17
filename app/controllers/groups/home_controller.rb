e9ce4229f2be676e4c9ac4176c3ca06919f2bab6
Adrien Liagre <adrienliagre@hotmail.fr>
2015-11-17 19:22:46 +0100

    added some style

--- app/controllers/groups/home_controller.rb
+++ app/controllers/groups/home_controller.rb
@@ -4,7 +4,6 @@ module Groups
       @newsfeed   = Newsfeed.new
       @group      = Group.friendly.find(params[:group])
       @newsfeeds  = @group.newsfeeds.order(created_at: :desc).limit(100)
-      @activity   = Activity.new
       @activities = @group.activities.planned
       @city       = params[:city]
       @sports     = Sport.all
@@ -15,34 +14,29 @@ module Groups
         @activities = @activities.joins(:location).where(locations: { id: @locations.map(&:id) })
 
         if @activities.empty?
-          flash[:notice] = "Désolé, il n'y a rien de prévu à #{@city} pour le moment !"
+          flash[:alert] = "Désolé, il n'y a rien de prévu à #{@city} pour le moment !"
           redirect_to group_root_path(current_user.group)
         end
-        # if @sport.present?
-        #   @activities = @activities.joins(:location).where(locations: { id: @locations.map(&:id) }) && @activities.join(:sports).where(sports: { id: @sports.map(&:id) })
-        # end
       end
 
       if @sport.present?
         @activities = @activities.where(sport: @sport)
-      else
-        flash[:notice] = "Désolé, il n'y a pas de #{@sport} pour le moment !"
-        @activities
+        if @activities.empty?
+          flash[:alert] = "Ce sport n'est pas prévu pour le moment!"
+          redirect_to group_root_path(current_user.group)
+        end
       end
 
 
 
 
-      @locations = Location.joins(:activities).where(activities: { id: @activities.pluck(:id) }) &&
+      @locations = Location.joins(:activities).where(activities: { id: @activities.pluck(:id) })
       # Let's DYNAMICALLY build the markers for the view.
       @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
         marker.lat location.latitude
         marker.lng location.longitude
         marker.infowindow render_to_string(partial: 'groups/shared/map_box', locals: { location: location })
       end
-
-
-
     end
   end
 end
