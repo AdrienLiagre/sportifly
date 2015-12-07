d5b2471353de0145ec04b12095ea14b494687da9
Théophile Lemaitre <theophile.lemaitre@gmail.com>
2015-12-07 10:28:26 +0100

    Added the inviter name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124132933) do
+ActiveRecord::Schema.define(version: 20151206181110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20151124132933) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "status"
+    t.string   "inviter"
   end
 
   add_index "bookings", ["activity_id"], name: "index_bookings_on_activity_id", using: :btree
