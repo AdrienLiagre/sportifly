16b2f133841ea677356ce5da3f7731ba6f077a47
Adrien Liagre <adrienliagre@hotmail.fr>
2016-03-18 15:32:35 +0800

    change active record version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151206181110) do
+ActiveRecord::Schema.define(version: 20151216092427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,16 @@ ActiveRecord::Schema.define(version: 20151206181110) do
   end
 
   add_index "comments", ["booking_id"], name: "index_comments_on_booking_id", using: :btree
+
+  create_table "contact_forms", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "company"
+    t.integer  "number"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "favorite_sports", force: :cascade do |t|
     t.integer  "sport_id"
