ee73e58d8a8792a26ca277ebb0b7b56bfc44a613
Adrien Liagre <adrienliagre@hotmail.fr>
2017-02-09 11:03:06 +0100

    fix notice when sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,4 +15,12 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  def after_sign_up_path_for(resource)
+      "/users/sign_in"
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+      "/users/sign_in"
+  end
+
 end
