c2140c9924bb068971c9c8f8b8c2fd72d1486fad
Adrien Liagre <adrienliagre@hotmail.fr>
2017-07-12 17:19:31 +0200

    2e essaie juste wagon setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'www.meetemps.io' }
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.default_url_options = { host: "www.meetemps.io" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
