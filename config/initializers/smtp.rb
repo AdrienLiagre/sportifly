4eac8681a93d8914a9a144e7ec39275043e8ca42
Adrien Liagre <adrienliagre@hotmail.fr>
2017-07-12 16:52:49 +0200

    change setting postmark again

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.postmarkapp.com',
   port: '25',
   domain: 'heroku.com',
-  user_name: ENV['POSTMARK_API_TOKEN'],
-  password: ENV['POSTMARK_API_TOKEN'],
+  user_name: ENV['POSTMARK_API_KEY'],
+  password: ENV['POSTMARK_API_KEY'],
   authentication: :cram_md5,
   enable_starttls_auto: true
 }
