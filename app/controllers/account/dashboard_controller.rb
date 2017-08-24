642df87875811dcfc7bc9f24e38359f2936c1990
napux <napux@debian>
2017-08-25 00:51:14 +0200

    stats quasi ok

--- app/controllers/account/dashboard_controller.rb
+++ app/controllers/account/dashboard_controller.rb
@@ -7,12 +7,20 @@ module Account
 
       if current_user.admin == true
 		time = Time.now.strftime("%d/%m/%Y %H:%M")
-		d = Date.parse(time)
-		d = (d<<1).to_s
-      	@activities_month = Activity.where(id: current_user.group_id).where("created_at>?", d).count
-      	@bookings_month = Booking.where(id: current_user.group_id).where("created_at>?", d).count      	
+    d1 = Date.parse(time)
+		d = (d1<<1).to_s
+    d2 = (d1<<2).to_s
+      	@activities_month = Activity.where(group_id: current_user.group_id).where("created_at>?", d).count
+        @activities_previous = Activity.where(group_id: current_user.group_id).where("created_at>?", d2).where("created_at<?", d).count        
+
+      	@bookings_month = Booking.where(id: current_user.group_id).where("created_at>?", d).count 
+        @bookings_previous = Booking.where(id: current_user.group_id).where("created_at>?", d2).where("created_at<?", d).count
+
       	@comments_month = Newsfeed.where(id: current_user.group_id).where("created_at>?", d).count
-      	@signin_month = User.where(id: current_user.group_id).where("current_sign_in_at>?", d).count
+        @comments_previous = Newsfeed.where(id: current_user.group_id).where("created_at>?", d2).where("created_at<?", d).count
+
+      	@signin_month = User.where(group_id: current_user.group_id).where("current_sign_in_at>?", d).count
+        @signin_previous = User.where(group_id: current_user.group_id).where("current_sign_in_at>?", d2).where("current_sign_in_at<?", d).count
       end
     end
   end
