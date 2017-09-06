18469ea3b61c9918adf245abd580696381d4f7bc
napux <napux@debian>
2017-09-06 23:31:29 +0200

    navbar correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
   config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -80,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.serve_static_assets = true
+  
 end
